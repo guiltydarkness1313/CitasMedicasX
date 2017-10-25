513b66f5fb94b3fa1600e2cabfacc13ae950ab0e
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2017-10-24 23:37:12 -0500

    Initial commit

---
+++ app/src/main/java/com/shibuyaxpress/citasmedicasx/IntroductionActivity.java
@@ -0,0 +1,38 @@
+package com.shibuyaxpress.citasmedicasx;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro2;
+
+public class IntroductionActivity extends AppIntro2 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addSlide(SliderActivity.newInstance(R.layout.slider_1));
+        addSlide(SliderActivity.newInstance(R.layout.slider_2));
+        addSlide(SliderActivity.newInstance(R.layout.slider_3));
+
+    }
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent launcher=new Intent(this,LoginActivity.class);
+        startActivity(launcher);
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+    }
+}
